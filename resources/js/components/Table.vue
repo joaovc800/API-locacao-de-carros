8e37acb72fa9d2af2d28f61fa65abf4345b3d557
João <joaoviniciusdacosta800@gmail.com>
2022-09-19 14:02:45 -0300

    Revert "teste"
    This reverts commit 02211b2472358d6ce153fc52bd60247bc91d6925.

---
+++ resources/js/components/Table.vue
@@ -0,0 +1,20 @@
+<template>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>#</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+
+}
+</script>
